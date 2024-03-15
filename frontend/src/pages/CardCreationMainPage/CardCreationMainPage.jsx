aab9968d6d635fa65da6b910915fbc4aa62abb48
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-03-15 15:56:18 -0400

    add page structure for creation goal page of card system

---
+++ frontend/src/pages/CardCreationMainPage/CardCreationMainPage.jsx
@@ -0,0 +1,9 @@
+function CardCreationMainPage() {
+    return ( 
+        <div className="card-crestion-main h-screen">
+            asd
+        </div>
+     );
+}
+
+export default CardCreationMainPage;
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 // card system
 import CardHomePage from './pages/CardHomePage/CardHomePage';
 import CardHistoryPage from './pages/CardHistoryPage/CardHistoryPage';
+import CardCreationGoalPage from './pages/CardCreationGoalPage/CardCreationGoalPage';
+import CardCreationMainPage from './pages/CardCreationMainPage/CardCreationMainPage';
 
 // mindmap system
 
@@ -16,14 +18,17 @@ export default function Routers(){
     return (
         <BrowserRouter>
             <Routes>
-            {/* Redirect from "/" to "/card/home" */}
-            <Route path="/" element={<Navigate replace to="/card/home" />} />
+                {/* Redirect from "/" to "/card/home". For easy development */}
+                <Route path="/" element={<Navigate replace to="/card/home" />} />
 
 
 
                 {/* card system */}
                 <Route path="/card/home" element={<CardHomePage/>} />
                 <Route path="/card/history" element={<CardHistoryPage/>} />
+                <Route path="/card/creation-goal" element={<CardCreationGoalPage/>} />
+                <Route path='/card/creation-main' element={<CardCreationMainPage/>} />
+                
 
 
 
