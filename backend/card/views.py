5b1759c6835e10e48d1159317c01143db7a3e902
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-03-15 17:49:55 -0400

    Update views.py

--- backend/card/views.py
+++ backend/card/views.py
@@ -3,5 +3,5 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import redirect
 
-# Do not write views here, write API views under /controllers
+# Do not write views here, create a file and write API views under /controllers.yourfile.py
      
