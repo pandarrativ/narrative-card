b8aba1f3550509999d75f29174b17aae812b8e6f
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-03-25 19:06:49 -0400

    add sliding carousel

--- frontend/src/components/CardSegmentWoodPanel/CardSegmentWoodPanel.jsx
+++ frontend/src/components/CardSegmentWoodPanel/CardSegmentWoodPanel.jsx
@@ -5,8 +5,8 @@ function CardSegmentWoodPanel({value, onValueChange, onClickSend, onClose }) {
         <div className="wood-panel wood-panel-expand">
             <img src={iconWoodPanel} alt="a wood panel" className="wood-panel-img"></img>
             <button className="wood-panel-mouse hover:cursor-pointer shadow-card font-monofett" onClick={onClose}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
             </button>
             <div className="wood-panel-body flex flex-col justify-between items-center">
---
+++ frontend/src/components/SlidingCarousel/SlidingCarousel.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./sliding-carousel.css";
+import defaultCard from "./imgs/default-card.png";
+
+function SlidingCarousel() {
+    const [centerIndex, setCenterIndex] = useState(0);
+    const [dataList, setDataList] = useState([0,1,2,3,4,5, 6, 7])
+
+    const calculateStyle = (index) => {
+        if(index === centerIndex){
+            return {x:"50%", y:"50%", z:dataList.length+1, scale:"22%"};
+        }else if(index < centerIndex){
+            let gap = 50/(centerIndex+1);
+            let x = gap*(index + 1);
+            let scale = 50 + 50/(centerIndex)*index;
+            return {x:`${x}%`, y:"50%", z:index+1, scale:`${0.22*scale}%`};
+        }else{
+            let gap = 50/(dataList.length - centerIndex);
+            let x = 100 - gap*(dataList.length - index);
+            let scale = 100 - 50/(dataList.length - centerIndex - 1)*(index - centerIndex);
+            return {x:`${x}%`, y:"50%", z:dataList.length - index + 1, scale:`${0.22*scale}%`};
+        }
+    }
+
+    // for(let i=0; i<dataList.length; i++){
+    //     console.log("cur:", centerIndex)
+    //     console.log(i, ": ", calculateStyle(i));
+    // }
+
+
+
+    return ( 
+        <div className="sliding-carousel">
+            {dataList.map((item, i) => {
+                let style = calculateStyle(i);
+                return (
+                    <div 
+                        className="sliding-carousel-card" 
+                        key={i} 
+                        style={{left:style.x, top:style.y, zIndex:style.z, width:style.scale, transition:"all 0.5s ease"}}
+                        onClick={() => setCenterIndex(i)}
+                    >
+                        <img src={defaultCard} alt="a green card"></img>
+                    </div>
+                )
+            })}
+        </div>
+     );
+}
+
+export default SlidingCarousel;
